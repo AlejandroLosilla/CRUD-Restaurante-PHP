81cb51faaeb6acf2d99ad913a089560e2c4c0bf2
AlejandroLosilla <50619416+AlejandroLosilla@users.noreply.github.com>
2021-05-19 02:09:43 +0200

    Update Restaurante.php

--- Restaurante.php
+++ Restaurante.php
@@ -25,7 +25,7 @@ $resultado = $conexion -> query ("SELECT * FROM MENU");
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>MENU</title>
-<link rel="stylesheet" type="text/css" href="estiloAgenda.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="estiloRestaurante.css" media="screen" />
 </head>
 
 <body>
