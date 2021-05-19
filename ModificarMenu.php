1842fb1c58c79d3d7e67893235c8712e27ea0971
AlejandroLosilla <50619416+AlejandroLosilla@users.noreply.github.com>
2021-05-19 02:17:08 +0200

    Create ModificarMenu.php

---
+++ ModificarMenu.php
@@ -0,0 +1,48 @@
+<?php
+//Parámetros de conexión con la base de datos
+$servername = "fdb27.runhosting.com";
+$basededatos = "3802606_wpressa36b517a";
+$username = "3802606_wpressa36b517a";
+$password = "Qwertyui1";
+
+// Crear conexion con la base de datos
+$conexion = new mysqli($servername, $username, $password, $basededatos);
+
+//Si no se puede conectar mensaje de error y salir
+if ($conexion->connect_error) {
+  die("ERROR: no se pudo conectar con la base de datos " . $conn->connect_error);
+}
+
+//Esto es para que utilice los caracteres con tildes
+$conexion->set_charset("utf8");
+
+//Se selecciona la fila cuyo nombre coincide con el parámetro pasado por GET
+$resultado = $conexion -> query ("SELECT * FROM MENU where plato=".$_GET['plato']);
+$fila = $resultado->fetch_array();
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+
+<h5>MODIFICAR MENU</h5>
+<form action="updateContacto.php"  method="get">  
+  <p>Plato: <input type="text" name="plato" readonly="readonly" value=<?php echo "'".$fila[0]."'"?> ></p>
+  <p>ID: <input type="text" name="id" value='<?php echo $fila[1]?>' ></p>
+  <p>Precio: <input type="text" name="precio" value='<?php echo $fila[2]?>' ></p>
+  <p><input type="submit" value="Modificar"></p>
+</form>
+
+
+</body>
+</html>
+
+<?php
+// Liberar resultados
+mysqli_free_result($resultado);
+
+// Cerrar la conexión
+mysqli_close($conexion);
+?>
